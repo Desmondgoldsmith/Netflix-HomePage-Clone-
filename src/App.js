fe38b660ab90a7416230d2137810fc5bfbb45e9b
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-20 16:01:11 +0000

    Setting Up 😍

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      {/* {enjoy tv section} */}
+      {/* Download and watch show section */}
+      {/* watch everywhere section */}
+      {/* create profiles for kids section */}
+      {/* FAQs */}
+      {/* footer */}
     </div>
   );
 }
