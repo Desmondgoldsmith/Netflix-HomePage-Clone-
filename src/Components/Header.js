ad21b6227d891c429b7b0d7f3f0da502fadeda26
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-20 19:22:54 +0000

    Working on the Header 🧨

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,17 +5,22 @@ function Header() {
   return (
     <header className='MainHeader' style={{
         backgroundPosition : "center center",
-        backgroundImage : `url("https://i.ibb.co/vXqDmnh/background.jpg")`,
+        // backgroundImage : `url("https://i.ibb.co/vXqDmnh/background.jpg")`,
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5),
+                       rgba(0, 0, 0, 0.5)), url("https://i.ibb.co/vXqDmnh/background.jpg")`,
         backgroundSize: "cover",
     }}>
-    <div className='dim'>
       <div className = 'header'>
         <img src = "https://i.ibb.co/r5krrdz/logo.png" alt='Netflix Logo'></img>
         <button>Sign In</button>
       </div>
+      <div className='some_text_'>
+        <h1 className='first--'>Unlimited movies, TV</h1>
+      <h2 className='second--'>shows, and more.</h2>
+      <h3 className='third--'>Watch anywhere. Cancel anytime.</h3>
+      <p className='fourth--'>Ready to watch? Enter your email to create or restart your membership.</p>
       </div>
-      <h1>Mad man</h1>
-    </header>
+      </header>
   )
 }
 
