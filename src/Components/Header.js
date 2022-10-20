7899d4f3b31c5a1b369bcab91a75660eb2bf92ce
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-20 16:07:27 +0000

    Working on the Header 🧨

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Header() {
   return (
-    <div className='header'>This is Neflix's Header</div>
+    <div className='header'>
+      <div className='logo'>
+        <img src='https://i.ibb.co/r5krrdz/logo.png' alt='Netflix Logo'></img>
+        <button className='bt-1'>Sign In</button>
+      </div>
+    </div>
   )
 }
 
