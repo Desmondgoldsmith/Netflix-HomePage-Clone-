12e2d041e91bcb02261c208bf3fa750a70d50337
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-21 12:46:32 +0000

    Adding some interesting stuff 🚀

--- src/Components/FAQ.js
+++ src/Components/FAQ.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './faq.css';
+import AddIcon from '@mui/icons-material/Add';
 
 function FAQ() {
   return (
     <div className='FAQ'>
        <div className='FAQs'>
             <h1 className='Question'>Frequently Asked Questions</h1>
-            <div className='Faq_container'><h3>What is Netflix?</h3></div>
+            <div className='Faq_container'><h3>What is Netflix? <AddIcon/></h3></div>
        </div>
         <div className='line'></div>
     </div>
