bdf43a00c4e575aaf6f0dc8dd61bc374339eda4c
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-21 03:34:08 +0000

    Adding some interesting stuff 🚀

--- src/Components/Download.js
+++ src/Components/Download.js
@@ -4,14 +4,13 @@ import './download.css';
 function Download() {
   return (
     <div className='Download'>
-     <div className='line'></div>
      <div className='details'>
      <img className='side_image' src='https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg' alt=''></img>
      <div className='title first--'>
-     <h1>Enjoy on your TV.</h1>
-    <p className='second--'>Watch on Smart TVs, Playstation, Xbox,</p>
-    <p className='second--'> Chromecast, Apple TV, Blu-ray players, and </p>
-    <p className='second--'> more.</p>
+     <h1>Download your shows </h1>
+     <h1>to watch offline. </h1>
+    <p className='second--'>Save your favorites easily and always have </p>
+    <p className='third--'> something to watch. </p>
     
     </div>
      </div>
