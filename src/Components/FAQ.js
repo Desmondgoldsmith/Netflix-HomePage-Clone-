3bff2a299feba7ab5de9ac8e287866fe9dad3b1e
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-21 12:15:58 +0000

    Adding some interesting stuff 🚀

---
+++ src/Components/FAQ.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './faq.css';
+
+function FAQ() {
+  return (
+    <div className='FAQ'>
+        <h1 className='Question'>Frequently Asked Questions</h1>
+    </div>
+  )
+}
+
+export default FAQ
