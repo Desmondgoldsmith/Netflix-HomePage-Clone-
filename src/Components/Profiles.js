adb4c01afdbe4a8c5de9925023b9db41a24de95a
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-21 03:51:26 +0000

    Adding some interesting stuff 🚀

--- src/Components/Profiles.js
+++ src/Components/Profiles.js
@@ -5,12 +5,12 @@ function Profiles() {
   return (
     <div className='Profiles'>
      <div className='details'>
-     <img className='side_image' src='https://assets.nflxext.com/ffe/siteui/acquisition/ourStory/fuji/desktop/mobile-0819.jpg' alt=''></img>
+     <img className='side_image' src='https://occ-0-55-116.1.nflxso.net/dnm/api/v6/19OhWN2dO19C9txTON9tvTFtefw/AAAABfpnX3dbgjZ-Je8Ax3xn0kXehZm_5L6-xe6YSTq_ucht9TI5jwDMqusWZKNYT8DfGudD0_wWVVTFLiN2_kaQJumz2iivUWbIbAtF.png?r=11f' alt=''></img>
      <div className='title first--'>
-     <h1>Download your shows </h1>
-     <h1>to watch offline. </h1>
-    <p className='second--'>Save your favorites easily and always have </p>
-    <p className='third--'> something to watch. </p>
+     <h1>Create profiles for kids. </h1>
+    <p className='second--'>Send kids on adventures with their favorite </p>
+    <p className='third--'> something to watcharacters in a space made just for them—</p>
+    <p className='third--'>free with your membership.ch. </p>
     
     </div>
      </div>
